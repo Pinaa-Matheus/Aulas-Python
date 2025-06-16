aadd889cbb891d1acc0d31a96e7e6903999c8ffb
Matheus Pina <m.pinadev@gmail.com>
2025-06-16 20:59:38 -0300

    Estruturas de repetição

---
+++ 3-Estruturas de repetição/1.Identação e blocos.py
@@ -0,0 +1,16 @@
+saldo = 500
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("Valor sacado!")                          #Esse dois estão dentro do if porque tem um espaço 
+        print("Retire o seu dinheiro na boca do caixa") #Os blocos em python são identificados por espaços
+
+print("Obrigado por ser nosso cliente") #Isso esta fora da função
+
+sacar(1000)
+
+def depositar(valor):
+saldo = 500
+saldo ** valor
+#Essa estrutura não vai funcionar por causa dos espaços, quando vc começa um if,def,ifelse o propio python ja coloca esses espaços
---
+++ 3-Estruturas de repetição/2.Estruturas_condicionais.py
@@ -0,0 +1,46 @@
+###############SO FAZENDO COM O IF################
+#saldo = 2000
+#saque = float(input("Informe o valor do saque: "))
+
+#if saldo >= saque:
+    #print("Realizando o saque!")
+
+#if saldo < saque:
+    #print("Saldo insuficiente!")
+##################################################
+import sys
+
+
+saldo = 2000
+saque = float(input("Informe o valor do saque: "))
+
+if saldo >= saque:
+    print("Realizando saque!")
+else:
+    print("Saldo insuficiente")
+#######################################################
+opcao = int(input("Informe uma opção: [1] sacar \n[2] Extrato: "))
+if opcao == 1:
+    valor = float(input("Informe a quantidade para o saque: "))
+elif opcao == 2:
+    print("Exibindo o extrato ....")
+else:
+    sys.exit("Opção inválida")
+##########################################
+#Tambem podemos ter if dentro de if
+if conta_normal: #Exemplo sistema de banco
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com uso do cheque especial")
+elif conta_universitaria:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    else:
+        print("Saldo insuficiente!")
+#######################################
+#If em uma linha, isso pode ser util em operaçoes simples
+status = "Sucesso" if saldo >= saque else "Falha"
+print(f"{status} ao realizar o saque!")
+
+#Se saldo for maior que o saque ele vai retornar Sucesso se não ele retorna Falha
